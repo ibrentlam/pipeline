49b794f3b5d2e6089ecef73d8abb2b14f8795d1a
Brent Laminack <brent@laminack.com>
2018-09-26 16:09:05 -0400

    fixed path

--- rungo.sh
+++ rungo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-PATH=$PATH:/usr/local/go/bin go test main_test.go main.go && go run main.go
+PATH=$PATH:/usr/local/go/bin go test pipeline/main_test.go pipeline/main.go && go run pipeline/main.go
