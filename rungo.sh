0a5a1056cac56444ca25878f46f4040bceb889ce
Brent Laminack <brent@laminack.com>
2018-09-26 16:11:23 -0400

    exported PATH

--- rungo.sh
+++ rungo.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-PATH=$PATH:/usr/local/go/bin go test pipeline/main_test.go pipeline/main.go && go run pipeline/main.go
+export PATH=$PATH:/usr/local/go/bin 
+go test pipeline/main_test.go pipeline/main.go && go run pipeline/main.go
